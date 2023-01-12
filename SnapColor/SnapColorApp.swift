60ebdbf9eeacbbf5cb30341b7624032b50a16356
ihealing <shifaabdulaziz@hotmail.com>
2023-01-12 11:42:35 +0300

    Project

--- SnapColor/SnapColorApp.swift
+++ SnapColor/SnapColorApp.swift
@@ -1,17 +1,18 @@
 //
-//  SnapColorApp.swift
-//  SnapColor
+//  SnapcolorApp.swift
+//  Snapcolor
 //
-//  Created by Shifa Alfaisal on 19/06/1444 AH.
+//  Created by Shifa Alfaisal on 11/06/1444 AH.
 //
 
 import SwiftUI
 
+
 @main
-struct SnapColorApp: App {
+struct SnapcolorApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Splash()
         }
     }
 }
